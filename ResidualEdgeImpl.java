6bed0ffd94759112dd19f9a5fd612c24d3564747
helenamehler <73898015+helenamehler@users.noreply.github.com>
2020-12-08 14:06:55 +0100

    Add files via upload

--- ResidualEdgeImpl.java
+++ ResidualEdgeImpl.java
@@ -3,35 +3,69 @@ package solutions.exercise3;
 import org.sopra.api.exercises.ExerciseSubmission;
 import org.sopra.api.exercises.exercise3.ResidualEdge;
 
-public class ResidualEdgeImpl<V> extends Edge<V> implements ResidualEdge<V>, ExerciseSubmission{
+/**
+ * This class describes an edge of a residual graph
+ * 
+ * @author G03T03
+ * 
+ */
+public class ResidualEdgeImpl<V> extends Edge<V> implements ResidualEdge<V>, ExerciseSubmission {
 
 	private ResidualEdge<V> reverseEdge;
-	
+
+	/**
+	 * Constructor which sets start + end node and capacity of the edge
+	 * 
+	 * @param startNode start node of the edge
+	 * @param endNode   end node of the edge
+	 * @param capacity  capacity of the edge
+	 */
 	public ResidualEdgeImpl(V startNode, V endNode, int capacity) {
 		this.startNode = startNode;
 		this.endNode = endNode;
 		this.capacity = capacity;
 	}
+
+	/**
+	 * adds a positive/negative flow to an edge by decreasing/increasing capacity
+	 * and increasing/decreasing capacity of reverse edge
+	 * 
+	 * @param amount the amount of flow to be added
+	 * @throws IllegalArgumentException if absolute of to be added flow is bigger
+	 *                                  than capacity
+	 */
 	public void addFlow(int amount) {
-		if(amount > capacity || amount < -capacity) {
+		if (Math.abs(amount) > capacity) {
 			throw new IllegalArgumentException("Amount is not allowed to be bigger than capacity");
 		}
 		capacity = capacity - amount;
-		reverseEdge.setCapacity(capacity + 2*amount); //Wieso???
-		
+		reverseEdge.setCapacity(reverseEdge.getCapacity() + amount);
+
 	}
+
+	/**
+	 * returns reverse edge
+	 * 
+	 * @return reverse edge of residual edge
+	 */
 
 	public ResidualEdge<V> getReverse() {
 		return reverseEdge;
 	}
 
+	/**
+	 * sets reverse edge
+	 * 
+	 * @param reverse the new reverse edge
+	 */
 	public void setReverse(ResidualEdge<V> reverse) {
-		reverseEdge = reverse;	
-		
+		reverseEdge = reverse;
+
 	}
-	
+
 	/**
 	 * Returns Team Identifier
+	 * 
 	 * @return String with team identifier
 	 */
 	public String getTeamIdentifier() {
